9ef9a4ae5fb29f4eca4577fdc74bac432462bfd6
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-03-04 10:36:41 +0600

    banner will change from the dashboard

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -6,19 +6,25 @@ import { Testimonials } from "@/components/testimonials";
 import { TeamSection } from "@/components/About/team-section";
 import { Banner2 } from "@/components/Banner/Banner2";
 
-
 export default function Home() {
+  type bannerActive = {
+    banner1: boolean;
+    banner2: boolean;
+  };
+  const bannerActive: bannerActive = {
+    banner1: true,
+    banner2: false,
+  } ;
   return (
     <>
       <main>
-        {/* <Banner2 /> */}
-        <Banner />
+        {bannerActive?.banner1 === true && <Banner />}
+        {bannerActive?.banner2 === true && <Banner2 />}
         <Services />
         <Features />
         <Gallery />
         <TeamSection />
         <Testimonials />
-   
       </main>
     </>
   );
