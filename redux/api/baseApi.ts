1aeaf1beeeeb24a44bf5379f02f48a1db4d190be
unknown <mdtanvir7462@gmail.com>
2025-06-08 18:06:29 +0600

    some changes

--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -20,3 +20,53 @@ export const baseAPi = createApi({
 
   endpoints: () => ({}),
 });
+
+
+
+
+// import { envConfig } from "@/lib/env.config";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { RootState } from "../features/store";
+// import { setUser } from "../features/auth/authSlice";
+// const BASE_URL = envConfig.SERVER_BASE_URL;
+// const baseQuery = fetchBaseQuery({
+//   baseUrl: BASE_URL,
+//   credentials: "include",
+//   prepareHeaders: (headers, { getState }) => {
+//     const token = (getState() as RootState).auth.token;
+//     if (token) {
+//       headers.set("authorization", token);
+//     }
+//     return headers;
+//   },
+// });
+
+
+// const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+//   const result = await baseQuery(args, api, extraOptions)
+  
+//   console.log(result?.data?.data,"error")
+//   if (result?.data?.error?.status === 403) {
+//     console.log("refresh....")
+//     const res = await fetch(`${BASE_URL}/auth/login/refresh-token`, {
+//       method: "POST",
+//       credentials: "include"
+//     })
+//     const newData = await res.json()
+//     console.log(newData,"new")
+//     const user = (api.getstate() as RootState).auth.user;
+//     api.dispatch(
+//       setUser({
+//         user, token: newData
+//     })
+//   )
+// }
+
+// }
+
+
+// export const baseAPi = createApi({
+//   reducerPath: "baseApi",
+//   baseQuery: baseQueryWithRefreshToken,
+//   endpoints: () => ({}),
+// });
